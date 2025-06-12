c7fb4e492f598f8082ef050fac411bc56565dbbb
Felipe <felipeolguera@gmail.com>
2025-06-12 21:43:43 +0400

    replaced images

--- src/App.js
+++ src/App.js
@@ -26,12 +26,12 @@ function Items() {
     <Scroll>
       <Item url="/1.jpg" scale={[w / 2.5, w/4.4 , 1]} position={[-w / 5, 0, 0]} />
       <Item url="/2.jpg" scale={[2, w / 3, 1]} position={[w / 30, -h, 0]} />
-      <Item url="/3.jpg" scale={[w / 3, w / 5, 1]} position={[-w / 4, -h * 1, 0]} />
+      <Item url="/3.jpg" scale={[w / 3, w / 5.6, 1]} position={[-w / 4, -h * 1, 0]} />
       <Item url="/4.jpg" scale={[w / 5, w / 5, 1]} position={[w / 4, -h * 1.2, 0]} />
       <Item url="/5.jpg" scale={[w / 5, w / 5, 1]} position={[w / 10, -h * 1.75, 0]} />
       <Item url="/6.jpg" scale={[w / 3, w / 3, 1]} position={[-w / 4, -h * 2, 0]} />
       <Item url="/7.jpg" scale={[w / 3, w / 5, 1]} position={[-w / 4, -h * 2.6, 0]} />
-      <Item url="/8.jpg" scale={[w / 2, w / 2, 1]} position={[w / 4, -h * 3.1, 0]} />
+      <Item url="/8.jpg" scale={[w / 2.09  , w / 3.85 , 1]} position={[w / 4, -h * 3.1, 0]} />
       <Item url="/12.jpg" scale={[w / 2.5, w / 2, 1]} position={[-w / 6, -h * 4.1, 0]} />
     </Scroll>
   )
